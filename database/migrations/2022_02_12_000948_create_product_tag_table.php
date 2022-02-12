14e76db40a1b54040abdf443d062bce4fc14922d
MarcusXavierr <marcusxavierr123@gmail.com>
2022-02-11 21:14:28 -0300

    Create migrations

---
+++ database/migrations/2022_02_12_000948_create_product_tag_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductTagTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_tag', function (Blueprint $table) {
+            $table->bigInteger('product_id');
+            $table->bigInteger('tag_id');
+            $table->primary(['product_id', 'tag_id']);
+
+            $table->foreign('product_id')->references('id')->on('product');
+            $table->foreign('tag_id')->references('id')->on('tags');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_tag');
+    }
+}
