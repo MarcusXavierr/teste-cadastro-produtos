874557c4edbd68035d7e445eaf513f210c3e9d01
MarcusXavierr <marcusxavierr123@gmail.com>
2022-02-12 12:31:00 -0300

    Add resource routes and auth routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::resource("produtos", '\App\Http\Controllers\ProductController');
+Route::resource("tags", "\App\Http\Controllers\TagController");
+
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
