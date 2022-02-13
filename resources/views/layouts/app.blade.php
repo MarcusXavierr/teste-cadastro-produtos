ac9024c85c132b8cd891de14fe5270ca701ebc42
MarcusXavierr <marcusxavierr123@gmail.com>
2022-02-13 16:32:37 -0300

    translate auth pages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -62,7 +62,7 @@
 
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Criar conta') }}</a>
                                 </li>
                             @endif
                         @else
