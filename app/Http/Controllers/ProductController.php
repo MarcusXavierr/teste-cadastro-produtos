0fbb45fdf4fc067bccba60a7dbe5d68c921d3237
MarcusXavierr <marcusxavierr123@gmail.com>
2022-02-12 22:28:42 -0300

    Add pagination

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -24,7 +24,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        $products = Product::all();
+        $products = Product::orderBy('name')->paginate(15);
 
         return view('products.index', compact('products'));
     }
--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -21,7 +21,7 @@ class TagController extends Controller
      */
     public function index()
     {
-        $tags = Tag::all();
+        $tags = Tag::paginate(15);
 
         return view('tag.index', compact("tags"));
     }
