3e0c29e339e431f3eeab1b53ab962cb37c7bd06b
MarcusXavierr <marcusxavierr123@gmail.com>
2022-02-12 22:46:02 -0300

    add notify css and js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,6 +21,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @notifyCss
     {{-- nem sempre é bom adicionar dessa forma, mas como não é um produto final vou usar assim --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
@@ -86,8 +87,10 @@
         </nav>
 
         <main class="py-4">
+            <x:notify-messages />
             @yield('content')
         </main>
     </div>
+    @notifyJs
 </body>
 </html>
